8b511698593c7521d12c7079f611cd2512e90c18
Seunghwan Choi <dadadak4@gmail.com>
2020-06-27 03:08:06 +0900

    Update train.py to connect GCN and RNN

--- train.py
+++ train.py
@@ -1,31 +1,30 @@
 from __future__ import division
 from __future__ import print_function
 
-import time
 import argparse
+import os
+import time
+
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.optim as optim
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from tqdm import tqdm
 
-from utils import *
-from models import GCN
-
-import networkx as nx
-import torch 
-from scipy.sparse import coo_matrix
-import scipy.sparse as sp
+from utils import CoauthorDataset, collate_fn, make_graph, make_random_adj, load_data
+from models import GCN, RNN
 
 
-
-### Training with batch adjacency matirx
-data_dir= './project_data/'
+# Training with batch adjacency matrix
+data_dir = './project_data/'
 paper_author = make_graph(data_dir+'paper_author.txt')
-A = make_random_adj(paper_author,nodelist = np.arange(10000))
+A = make_random_adj(paper_author, nodelist=np.arange(10000))
 # print(A.shape)
 
 label = torch.randn([58646])
-label = label>-0.2
+label = label > -0.2
 # print(label.float())
 # print(paper_author.number_of_nodes())
 # print(paper_author.number_of_edges())
@@ -33,34 +32,31 @@ label = label>-0.2
 
 # Training settings
 parser = argparse.ArgumentParser()
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='Disables CUDA training.')
-parser.add_argument('--fastmode', action='store_true', default=False,
-                    help='Validate during training pass.')
+
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=200,
-                    help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.01,
-                    help='Initial learning rate.')
-parser.add_argument('--weight_decay', type=float, default=5e-4,
-                    help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=200,
-                    help='Number of hidden units.')
-parser.add_argument('--dropout', type=float, default=0.5,
-                    help='Dropout rate (1 - keep probability).')
-parser.add_argument('--features', type=int, default=256,
-                    help='node_2_vec_feature_dim')
-parser.add_argument('--model', type=str, default='adj',
-                    help='Choosing between the adj and the node2vec')
+parser.add_argument('--name', type=str, required=True)
+parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/')
 
-parser.add_argument('--load_node2vec', type=bool, default=True,
-                    help='load precalculated node2vec object')
+parser.add_argument('--epochs_pretrain', type=int, default=50, help='Number of epochs to pretrain.')
+parser.add_argument('--epochs_train', type=int, default=100, help='Number of epochs to train.')
+parser.add_argument('-b', '--batch_size', type=int, default=128)
+parser.add_argument('-j', '--workers', type=int, default=4)
 
-#### node2vec setting
-parser.add_argument('--walk_length', type=int, default=50,
-                    help=' node2vec walk_length')
-parser.add_argument('--num_walks', type=int, default=4,
-                    help=' node2vec num_walks')
+parser.add_argument('--lr', type=float, default=0.01, help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
+
+parser.add_argument('--feature_node', type=int, default=256, help='node_2_vec_feature_dim')
+parser.add_argument('--hidden_gcn', type=int, default=200)
+parser.add_argument('--feature_gcn', type=int, default=128)
+parser.add_argument('--hidden_rnn', type=int, default=32)
+parser.add_argument('--model', type=str, default='adj', help='Choosing between the adj and the node2vec')
+
+# node2vec setting
+parser.add_argument('--walk_length', type=int, default=80, help='node2vec walk_length')
+parser.add_argument('--num_walks', type=int, default=10, help='node2vec num_walks')
 
 
 args = parser.parse_args()
@@ -72,63 +68,92 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 
-
 # Load data
-adj, features = load_data(args = args)
+adj, features = load_data(args=args)
 # bulid symmetric adj matrix
 
 print(adj.shape, features.shape)
 
-# Model and optimizer
-if args.model =='adj':
-    model = GCN(nfeat=features.size()[1],
-                nhid=args.hidden,
-                nclass=2,
-                dropout=args.dropout)
-elif args.model =='node2vec':
-    model = GCN(nfeat=args.features,
-                nhid=args.hidden,
-                nclass=2,
-                # nclass=labels.max().item() + 1,
-                dropout=args.dropout)
+pretrain_dataset = CoauthorDataset('paper_author.txt')
+pretrain_loader = DataLoader(pretrain_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, collate_fn=collate_fn)
 
-optimizer = optim.Adam(model.parameters(),
-                       lr=args.lr, weight_decay=args.weight_decay)
+train_dataset = CoauthorDataset('query_public.txt')
+val_dataset = CoauthorDataset('query_public.txt')
+indices = list(range(len(train_dataset)))
+train_sampler = SubsetRandomSampler(indices[:int(len(train_dataset) * 0.9)])
+val_sampler = SubsetRandomSampler(indices[int(len(train_dataset) * 0.9):])
+
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, sampler=train_sampler, num_workers=args.workers, collate_fn=collate_fn)
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, sampler=val_sampler, num_workers=args.workers, collate_fn=collate_fn)
+
+# Model and optimizer
+if args.model == 'adj':
+    model = GCN(nfeat=features.size()[1],
+                nhid=args.hidden_gcn,
+                nclass=args.feature_gcn,
+                dropout=args.dropout)
+elif args.model == 'node2vec':
+    model = GCN(nfeat=args.feature_node,
+                nhid=args.hidden_gcn,
+                nclass=args.feature_gcn,
+                dropout=args.dropout)
+rnn = RNN(args.feature_gcn, args.hidden_rnn)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(list(model.parameters()) + list(rnn.parameters()), lr=args.lr, weight_decay=args.weight_decay)
 
 if args.cuda:
     model.cuda()
+    rnn.cuda()
     features = features.cuda()
     adj = adj.cuda()
-    # labels = labels.cuda()
-    # idx_train = idx_train.cuda()
-    # idx_val = idx_val.cuda()
-    # idx_test = idx_test.cuda()
 
 
-def train(epoch):
+def train(epoch, epochs, is_pretrain=False):
     t = time.time()
     model.train()
-    optimizer.zero_grad()
-    output = model(features, adj)
-    # loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-    # acc_train = accuracy(output[idx_train], labels[idx_train])
-    # loss_train.backward()
-    # optimizer.step()
+    rnn.train()
 
-    # if not args.fastmode:
-    #     # Evaluate validation set performance separately,
-    #     # deactivates dropout during validation run.
-    #     model.eval()
-    #     output = model(features, adj)
+    if is_pretrain:
+        real_label = torch.ones(1).cuda()
+        train_bar = tqdm(pretrain_loader)
+    else:
+        train_bar = tqdm(train_loader)
+    for queries, labels in train_bar:
+        queries = queries.cuda()
+        if not is_pretrain:
+            labels = labels.cuda()
+        embedding = model(features, adj)
+        embedding = F.pad(embedding, (0, 0, 1, 0), 'constant', 0)
+        logits = rnn(queries, embedding)
 
-    # loss_val = F.nll_loss(output[idx_val], labels[idx_val])
-    # acc_val = accuracy(output[idx_val], labels[idx_val])
-    # print('Epoch: {:04d}'.format(epoch+1),
-    #       'loss_train: {:.4f}'.format(loss_train.item()),
-    #       'acc_train: {:.4f}'.format(acc_train.item()),
-        #   'loss_val: {:.4f}'.format(loss_val.item()),
-        #   'acc_val: {:.4f}'.format(acc_val.item()),
-        #   'time: {:.4f}s'.format(time.time() - t))
+        if is_pretrain:
+            labels = real_label.expand_as(logits)
+        loss = criterion(logits, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_bar.set_description("[{}/{}] loss: {:.4f}".format(epoch, epochs, loss))
+
+    if not args.fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        rnn.eval()
+
+        with torch.no_grad():
+            embedding = model(features, adj)
+            embedding = F.pad(embedding, (0, 0, 1, 0), 'constant', 0)
+            val_bar = tqdm(val_loader)
+            val_results = {'correct': 0, 'num_queries': 0}
+            for queries, labels in val_bar:
+                queries = queries.cuda()
+                labels = labels.cuda()
+                logits = rnn(queries, embedding)
+
+                val_results['correct'] += torch.sum((logits > 0.0) == labels.byte()).item()
+                val_results['num_queries'] += queries.size(1)
+                val_bar.set_description("acc: {:4f}".format(val_results['correct'] / val_results['num_queries']))
 
 
 # def test():
@@ -141,12 +166,21 @@ def train(epoch):
 #           "accuracy= {:.4f}".format(acc_test.item()))
 
 
-# Train model
-t_total = time.time()
-for epoch in range(args.epochs):
-    train(epoch)
-print("Optimization Finished!")
-print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+if __name__ == '__main__':
+    if not os.path.exists(os.path.join(args.checkpoint_dir, args.name)):
+        os.makedirs(os.path.join(args.checkpoint_dir, args.name))
+    torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, args.name, 'GCN.ckpt'))
+    torch.save(rnn.state_dict(), os.path.join(args.checkpoint_dir, args.name, 'RNN.ckpt'))
+
+    # Train model
+    t_total = time.time()
+    for epoch in range(args.epochs_pretrain):
+        train(epoch + 1, args.epochs_pretrain, is_pretrain=True)
+    for epoch in range(args.epochs_train):
+        train(epoch + 1, args.epochs_train)
+
+    print("Optimization Finished!")
+    print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
 # Testing
 # test()
