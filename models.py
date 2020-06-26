f3dd2493057147993788b088068bb3f3a311632c
Seunghwan Choi <dadadak4@gmail.com>
2020-06-27 03:06:34 +0900

    Add RNN

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
-import torch.nn as nn
-import torch.nn.functional as F
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn.utils.rnn import pack_padded_sequence
+
 from layers import GraphConvolution
 
 
@@ -36,3 +39,23 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
+
+
+class RNN(nn.Module):
+    def __init__(self, embedding_size, hidden_size):
+        super(RNN, self).__init__()
+
+        self.cell = nn.LSTM(embedding_size, hidden_size, num_layers=2)
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size // 4, bias=False),
+            nn.Tanh(),
+            nn.Linear(hidden_size // 4, 1, bias=False)
+        )
+
+    def forward(self, x, embedding):
+        input_lengths = [torch.max(x[:, i].data.nonzero()) + 1 for i in range(x.size(1))]
+        sequence = pack_padded_sequence(embedding[x], lengths=input_lengths)
+        _, (h_n, _) = self.cell(sequence)
+        output = self.fc(h_n[-1])
+
+        return output.squeeze()
